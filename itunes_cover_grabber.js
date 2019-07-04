d1b2e55153f223ffb1489ac98483e1a7a96df924
Jay Somedon <jay.somedon@outlook.com>
2019-07-04 21:26:59 +0800

    refactor using promise based workflow
    Signed-off-by: Jay Somedon <jay.somedon@outlook.com>

--- itunes_cover_grabber.js
+++ itunes_cover_grabber.js
@@ -24,55 +24,54 @@
  * so when you click that button it shows you the album cover image with max resolution
  */
 
-function picture() {
-    return document.querySelectorAll("picture.we-artwork.product-artwork--captioned")[0];
-}
+const picture = () =>
+    document.querySelectorAll("picture.we-artwork.product-artwork--captioned")[0];
 
-function sources(picture) {
-    return Array.from(picture.children).filter(elem => elem.tagName == "SOURCE").filter(elem => elem.hasAttribute("media"));
-}
+const sources = (picture) =>
+    Array.from(picture.children).filter(elem => elem.tagName == "SOURCE").filter(elem => elem.hasAttribute("media"));
 
-function source_of_max_width(sources) {
-    var width = function (source) {
-        var reg = /\d+/;
-        return parseInt(source.media.match(reg));
-    }
-    var greater_width = function (acc, cur) {
-        return (width(acc) > width(cur)) ? acc : cur;
-    }
-    return sources.reduce(greater_width);
-}
+const width = (source) =>
+    parseInt(source.media.match(/\d+/));
 
-function cover_url(source) {
-    var url_zoom_pairs = source.srcset.split(',').map(url => url.split(' '));
-    var greater_url_zoom_pair = function (acc, cur) {
-        return acc[1] > cur[1] ? acc : cur;
-    }
-    return url_zoom_pairs.reduce(greater_url_zoom_pair)[0];
-}
+const wider = (acc, cur) =>
+    (width(acc) > width(cur)) ? acc : cur;
 
-function button_to_cover() {
-    var button = document.createElement("button");
-    button.onclick = function () {
-        window.open(cover_url(source_of_max_width(sources(picture()))));
-    };
+const widest_source = (sources) =>
+    sources.reduce(wider);
+
+const greater_url_zoom_pair = (acc, cur) =>
+    acc[1] > cur[1] ? acc : cur;
+
+const cover_url = (source) =>
+    source.srcset.split(',').map(urlzoomstr => urlzoomstr.split(' ')).reduce(greater_url_zoom_pair)[0];
+
+const button_to_url = (url) => {
+    const button = document.createElement("button");
+    button.onclick = () =>
+        window.open(url);
     return button;
-}
+};
 
-function style_button(button) {
+const style_button = (button) => {
     button.textContent = "click for cover";
     button.style.cssText = "position: fixed; margin: 3px; bottom: 0; left: 0; z-index: 99999; background-color: darkcyan; color: lightyellow; width: 100px; height: 100px; border-radius: 50px; font-size: 20px; text-align: center;";
-    button.onmouseover = function () {
-        this.style.background = "salmon";
-    }
-    button.onmouseout = function () {
-        this.style.background = "darkcyan";
-    }
+    button.onmouseover = (e) =>
+        e.target.style.background = "salmon";
+    button.onmouseout = (e) =>
+        e.target.style.background = "darkcyan";
     return button;
-}
+};
 
-function main() {
-    document.body.appendChild(style_button(button_to_cover()));
-}
+const render = (button) => document.body.appendChild(button);
+
+const main = () =>
+    Promise.resolve()
+        .then(picture)
+        .then(sources)
+        .then(widest_source)
+        .then(cover_url)
+        .then(button_to_url)
+        .then(style_button)
+        .then(render);
 
 main();
